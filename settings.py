59a7d385c67a14176d13604b6d868f35550533f7
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-04-28 15:42:17 -0400

    Added final theme settings for the hud font button and audio

--- settings.py
+++ settings.py
@@ -5,6 +5,12 @@ Purpose: Store the settings for the Alien Invasion game.
 Starter Code: Based on the Alien Invasion project from class and
 Python Crash Course, 3rd Edition by Eric Matthes.
 Date: 04/14/2026
+
+Asset Attribution:
+Font: Macondo and Macondo Swash Caps
+Author: John Vargas Beltrán
+Source/Link: Google Fonts
+
 '''
 
 from pathlib import Path
@@ -35,11 +41,36 @@ class Settings:
         self.fleet_drop_speed = 18
         self.fleet_direction = 1
 
+        self.ships_limit = 3
+        self.alien_points = 50
+
+        self.hud_text_color = (220, 255, 225)
+        self.hud_bg_color = (12, 28, 24)
+        self.hud_border_color = (105, 220, 170)
+
+        self.button_width = 260
+        self.button_height = 100
+        self.button_text_color = (245, 255, 230)
+
+        self.font_size = 28
+        self.button_font_size = 42
+
         self.base_dir = Path(__file__).resolve().parent
         self.assets_dir = self.base_dir / "Assets"
         self.images_dir = self.assets_dir / "images"
+        self.fonts_dir = self.assets_dir / "Fonts" / "Macondo"
+        self.sound_dir = self.assets_dir / "sound"
 
         self.ship_image = self.images_dir / "ship.png"
         self.bullet_image = self.images_dir / "laserBlast.png"
         self.background_image = self.images_dir / "Starbasesnow.png"
         self.alien_image = self.images_dir / "alien.png"
+        self.play_button_image = self.images_dir / "playButton.png"
+
+        self.font_path = self.fonts_dir / "Macondo-Regular.ttf"
+        self.button_font_path = self.fonts_dir / "MacondoSwashCaps-Regular.ttf"
+
+        self.music_file = self.sound_dir / "backgroundMusic.mp3"
+        self.leaves_sound = self.sound_dir / "leavesRustle03.mp3"
+        self.gun_sound = self.sound_dir / "sciFiGunShot.mp3"
+        self.impact_sound = self.sound_dir / "impactSound.mp3"
