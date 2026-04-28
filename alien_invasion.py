c37580467039b22c0814633be9370465d6d13ffb
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-04-27 21:04:57 -0400

    put in the fleet movement and edge drops so the swarm acts alive

--- alien_invasion.py
+++ alien_invasion.py
@@ -122,6 +122,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(self.settings.fps)
 
@@ -183,6 +184,24 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self) -> None:
+        '''Update the positions of all aliens in the fleet.'''
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _check_fleet_edges(self) -> None:
+        '''Respond appropriately if any aliens have reached an edge.'''
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self) -> None:
+        '''Drop the fleet and change the fleet's direction.'''
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+
+        self.settings.fleet_direction *= -1
 
 if __name__ == "__main__":
     ai = AlienInvasion()
